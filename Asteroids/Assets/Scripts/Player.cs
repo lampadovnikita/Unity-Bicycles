94ec2aecf84fe854d4e7c7a24787b53bb7086f8d
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-02 17:44:24 +0700

    Add primitive screen wrapped Spaceship behavior

--- Asteroids/Assets/Scripts/Player.cs
+++ Asteroids/Assets/Scripts/Player.cs
@@ -2,11 +2,19 @@
 
 public class Player : MonoBehaviour
 {
+    private static int halfHeight = Screen.height / 2;
+    private static int halfWidth = Screen.width / 2;
+
     private Rigidbody2D _playerRigidbody2D;
 
     private void Awake()
     {
         _playerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnBecameInvisible()
+    {
+        Vector2 position = _playerRigidbody2D.position;
     }
 
     public void AddRelativeForce(ForceDirection forceDirection)
---
+++ Asteroids/Assets/Scripts/ScreenWrapped.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ScreenWrapped : MonoBehaviour
+{
+    private Vector2 _screenBounds;
+
+    private Vector3 _topRight;
+    private Vector3 _bottomLeft;
+
+    private float _spriteWidth;
+    private float _spriteHeight;
+
+    private void Start()
+    {
+        float zDistance = transform.position.z - Camera.main.transform.position.z;
+        _topRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, zDistance));
+
+        _bottomLeft = new Vector3(-1 * (_topRight.x / 2), -1 * (_topRight.y / 2), _topRight.z);
+
+        _spriteWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x * 0.5f;
+        _spriteHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y * 0.5f;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 position = transform.position;
+
+        if (Mathf.Abs(position.x) > _topRight.x + _spriteWidth) {
+            transform.position = new Vector2(-position.x, position.y);
+        }
+        else if (Mathf.Abs(position.y) > _topRight.y + _spriteHeight) {
+            transform.position = new Vector2(-position.x, -position.y);
+        }
+    }
+}
