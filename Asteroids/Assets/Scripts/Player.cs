536d2f7cce594eab17e971cdf51b75d6a7f57ba6
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-02 20:17:44 +0700

    Refactor

--- Asteroids/Assets/Scripts/Player.cs
+++ Asteroids/Assets/Scripts/Player.cs
@@ -9,11 +9,6 @@ public class Player : MonoBehaviour
         _playerRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    private void OnBecameInvisible()
-    {
-        Vector2 position = _playerRigidbody2D.position;
-    }
-
     public void AddRelativeForce(ForceDirection forceDirection)
     {
         _playerRigidbody2D.AddRelativeForce(30f * forceDirection.GetForce());
