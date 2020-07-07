f5917ca78b3c24eebe431d3ea5500ef41fc6bf72
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-07 19:55:57 +0700

    Change approach to store boundary values

--- Asteroids/Assets/Scripts/WraparoundBehaviour.cs
+++ Asteroids/Assets/Scripts/WraparoundBehaviour.cs
@@ -2,11 +2,10 @@
 
 public class WraparoundBehaviour : MonoBehaviour
 {
-    private Vector3 _topRight;
-    private Vector3 _bottomLeft;
-
-    private float _spriteWidth;
-    private float _spriteHeight;
+    private float _upperBound;
+    private float _rightBound;
+    private float _bottomBound;
+    private float _leftBound;
 
     private Rigidbody2D _rigidbody2D;
 
@@ -19,36 +18,36 @@ public class WraparoundBehaviour : MonoBehaviour
 
         float zDistance = transform.position.z - Camera.main.transform.position.z;
 
-        _bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, zDistance));
-        _topRight = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, zDistance));
+        Vector3 _bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, zDistance));
+        Vector3 _topRight = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, zDistance));
 
-        _spriteWidth = spriteRenderer.bounds.size.x * 0.5f;
-        _spriteHeight = spriteRenderer.bounds.size.y * 0.5f;
+        float _spriteWidth = spriteRenderer.bounds.size.x * 0.5f;
+        float _spriteHeight = spriteRenderer.bounds.size.y * 0.5f;
+
+        _rightBound = _topRight.x + _spriteWidth;
+        _leftBound = _bottomLeft.x - _spriteWidth;
+
+        _upperBound = _topRight.y + _spriteHeight;
+        _bottomBound = _bottomLeft.y - _spriteHeight;
     }
 
     private void LateUpdate()
     {
         Vector3 position = transform.position;
 
-        float rightBound = _topRight.x + _spriteWidth;
-        float leftBound = _bottomLeft.x - _spriteWidth;
-
-        float upperBound = _topRight.y + _spriteHeight;
-        float bottomBound = _bottomLeft.y - _spriteHeight;
-
         // Position should be swapped to a bound coordinate instead of inverted position of an object
         // because that approach prevent bug with endless position inversion of an object that out of the screen area
-        if (position.x > rightBound) {
-            _rigidbody2D.position = new Vector2(leftBound, position.y);
+        if (position.x > _rightBound) {
+            _rigidbody2D.position = new Vector2(_leftBound, position.y);
         }
-        else if (position.x < leftBound) {
-            _rigidbody2D.position = new Vector2(rightBound, position.y);
+        else if (position.x < _leftBound) {
+            _rigidbody2D.position = new Vector2(_rightBound, position.y);
         }
-        else if (position.y > upperBound) {
-            _rigidbody2D.position = new Vector2(position.x, bottomBound);
+        else if (position.y > _upperBound) {
+            _rigidbody2D.position = new Vector2(position.x, _bottomBound);
         }
-        else if (position.y < bottomBound) {
-            _rigidbody2D.position = new Vector2(position.x, upperBound);
+        else if (position.y < _bottomBound) {
+            _rigidbody2D.position = new Vector2(position.x, _upperBound);
         }
     }
 }
