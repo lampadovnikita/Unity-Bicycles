e60e05194dfd658e615079f6601d15ccdda3f9a8
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-09 16:36:29 +0700

    Add Main Menu

---
+++ Asteroids/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void StartGame()
+	{
+		SceneManager.LoadScene("Game");
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
