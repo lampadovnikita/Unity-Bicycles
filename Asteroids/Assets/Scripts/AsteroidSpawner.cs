4306746c5f79414e73210b91b9c1f6f579bea30f
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-10 15:24:55 +0700

    Add separate class to asteroid spawner

---
+++ Asteroids/Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+	[SerializeField]
+	private AsteroidBehavior asteroidPrefab = default;
+
+	[SerializeField]
+	private int asteroidsSpawnLimit = 10;
+
+	[SerializeField, Range(0.0001f, 0.5f)]
+	private float xSpawnPaddingFactor = 0.1f;
+
+	[SerializeField, Range(0.0001f, 0.5f)]
+	private float ySpawnPaddingFactor = 0.1f;
+
+	private float xSpawnPadding = default;
+	private float ySpawnPadding = default;
+
+	private Vector3 topRightBound = default;
+	private Vector3 bottomLeftBound = default;
+
+	private void Start()
+	{
+		float zDistance = transform.position.z - Camera.main.transform.position.z;
+
+		bottomLeftBound = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, zDistance));
+		topRightBound = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, zDistance));
+
+		xSpawnPadding = xSpawnPaddingFactor * Mathf.Abs(topRightBound.x - bottomLeftBound.x);
+		ySpawnPadding = ySpawnPaddingFactor * Mathf.Abs(topRightBound.y - bottomLeftBound.y);
+	}
+
+	public int SpawnAsteroids(AsteroidBehavior.OnAsteroidDestroy onAsteroidDestroy)
+	{
+		Vector3 spawnPosition = new Vector3();
+		spawnPosition.z = transform.position.z;
+
+		AsteroidBehavior asteroid;
+
+		float xIndent;
+		float yIndent;
+
+		for (int i = 0; i < asteroidsSpawnLimit; i++)
+		{
+			// Choose the side along to which we wil generate a position.
+			// The generated position locates near chosen axis with some padding,
+			// so there some area around the border to spawn objects.
+			// if the RNG produce 0, generate the position along the y axis;
+			// else (if the RNG produce 1), generate the position along the x axis.
+			if (Random.Range(0, 2) == 0)
+			{
+				xIndent = Random.Range(-1 * xSpawnPadding, xSpawnPadding);
+				if (xIndent > 0)
+				{
+					spawnPosition.x = bottomLeftBound.x + xIndent;
+				}
+				else
+				{
+					// The indent is negative, so the addition move the objects position to the left
+					// of the screen.
+					spawnPosition.x = topRightBound.x + xIndent;
+				}
+
+				spawnPosition.y = Random.Range(bottomLeftBound.y, topRightBound.y);
+			}
+			else
+			{
+				yIndent = Random.Range(-1 * ySpawnPadding, ySpawnPadding);
+				if (yIndent > 0)
+				{
+					spawnPosition.y = bottomLeftBound.y + yIndent;
+				}
+				else
+				{
+					// The indent is negative, so the addition move the objects position to the bottom
+					// of the screen.
+					spawnPosition.y = topRightBound.y + yIndent;
+				}
+
+				spawnPosition.x = Random.Range(bottomLeftBound.x, topRightBound.x);
+			}
+
+			asteroid = Instantiate(asteroidPrefab, spawnPosition, Quaternion.identity);
+
+			if (onAsteroidDestroy != null)
+			{ 
+				asteroid.onAsteroidDestroyCallback += onAsteroidDestroy;			
+			}
+		}
+
+		return asteroidsSpawnLimit;
+	}
+}
--- Asteroids/Assets/Scripts/Game.cs
+++ Asteroids/Assets/Scripts/Game.cs
@@ -13,16 +13,7 @@ public class Game : MonoBehaviour
 	private int playerOriginLifes = 1;
 
 	[SerializeField]
-	private AsteroidBehavior asteroidPrefab = default;
-
-	[SerializeField]
-	private int asteroidsSpawnLimit = 10;
-
-	[SerializeField, Range(0.0001f, 0.5f)]
-	private float xSpawnPaddingFactor = 0.1f;
-
-	[SerializeField, Range(0.0001f, 0.5f)]
-	private float ySpawnPaddingFactor = 0.1f;
+	private AsteroidSpawner asteroidSpawner = default;
 
 	[SerializeField]
 	private GameObject gameOverUI = default;
@@ -39,31 +30,17 @@ public class Game : MonoBehaviour
 
 	private int activeAsteroidsCount = 0;
 
-	private float xSpawnPadding = default;
-	private float ySpawnPadding = default;
-
-	private Vector3 topRightBound = default;
-	private Vector3 bottomLeftBound = default;
-
 	private bool isGamePaused = false;
 
 	private void Start()
 	{
-		float zDistance = transform.position.z - Camera.main.transform.position.z;
-
-		bottomLeftBound = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, zDistance));
-		topRightBound = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, zDistance));
-
-		xSpawnPadding = xSpawnPaddingFactor * Mathf.Abs(topRightBound.x - bottomLeftBound.x);
-		ySpawnPadding = ySpawnPaddingFactor * Mathf.Abs(topRightBound.y - bottomLeftBound.y);
-
 		player.onPlayerDestroyCallback += OnPlayerDestroy;
 
 		playerLifes = playerOriginLifes;
 
 		scoreTextMeshPro.text = score.ToString();
 
-		SpawnAsteroids();
+		activeAsteroidsCount = asteroidSpawner.SpawnAsteroids(OnAsteroidDestroy);
 	}
 
 	private void Update()
@@ -122,63 +99,6 @@ public class Game : MonoBehaviour
 		pauseUI.SetActive(false);
 	}
 
-	private void SpawnAsteroids()
-	{
-		Vector3 spawnPosition = new Vector3();
-		spawnPosition.z = transform.position.z;
-
-		AsteroidBehavior asteroid;
-
-		float xIndent;
-		float yIndent;
-
-		for (int i = 0; i < asteroidsSpawnLimit; i++)
-		{
-			// Choose the side along to which we wil generate a position.
-			// The generated position locates near chosen axis with some padding,
-			// so there some area around the border to spawn objects.
-			// if the RNG produce 0, generate the position along the y axis;
-			// else (if the RNG produce 1), generate the position along the x axis.
-			if (Random.Range(0, 2) == 0)
-			{
-				xIndent = Random.Range(-1 * xSpawnPadding, xSpawnPadding);
-				if (xIndent > 0)
-				{
-					spawnPosition.x = bottomLeftBound.x + xIndent;
-				}
-				else
-				{
-					// The indent is negative, so the addition move the objects position to the left
-					// of the screen.
-					spawnPosition.x = topRightBound.x + xIndent;
-				}
-
-				spawnPosition.y = Random.Range(bottomLeftBound.y, topRightBound.y);
-			}
-			else
-			{
-				yIndent = Random.Range(-1 * ySpawnPadding, ySpawnPadding);
-				if (yIndent > 0)
-				{
-					spawnPosition.y = bottomLeftBound.y + yIndent;
-				}
-				else
-				{
-					// The indent is negative, so the addition move the objects position to the bottom
-					// of the screen.
-					spawnPosition.y = topRightBound.y + yIndent;
-				}
-
-				spawnPosition.x = Random.Range(bottomLeftBound.x, topRightBound.x);
-			}
-
-			asteroid = Instantiate(asteroidPrefab, spawnPosition, Quaternion.identity);
-			asteroid.onAsteroidDestroyCallback = OnAsteroidDestroy;
-
-			activeAsteroidsCount++;
-		}
-	}
-
 	private void OnPlayerDestroy()
 	{
 		playerLifes--;
@@ -198,7 +118,7 @@ public class Game : MonoBehaviour
 
 		if (activeAsteroidsCount == 0)
 		{
-			SpawnAsteroids();
+			activeAsteroidsCount = asteroidSpawner.SpawnAsteroids(OnAsteroidDestroy);
 		}
 	}
 }
