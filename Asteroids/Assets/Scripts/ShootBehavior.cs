5e5b70593551a680276b0b3cd4f2c99198f9116e
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-12 18:37:12 +0700

    Add shooting sound

--- Asteroids/Assets/Scripts/ShootBehavior.cs
+++ Asteroids/Assets/Scripts/ShootBehavior.cs
@@ -5,8 +5,30 @@ public class ShootBehavior : MonoBehaviour
 	[SerializeField]
 	private BulletBehavior bulletPrefab = default;
 
+	[SerializeField]
+	private AudioClip shootAudioClip = default;
+
+	private AudioSource audioSource = default;
+
+	public void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
 	public void Shoot()
 	{
 		Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+		if (shootAudioClip != null)
+		{
+			if (audioSource != null)
+			{
+				audioSource.PlayOneShot(shootAudioClip, 0.5f);
+			}
+			else
+			{
+				Debug.Log("Audio source is equal to null, but audio clip exist");
+			}
+		}
 	}
 }
