1a1c98d0fb73a5082d6c0a0dfefec07f28df9bce
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-12 23:22:43 +0700

    Add singleton AudioManager

---
+++ Asteroids/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	public static AudioManager Instance { get; private set; }
+
+	[SerializeField]
+	private AudioSource _globalAudioSource;
+
+	public AudioSource GlobalAudioSource
+	{
+		get
+		{
+			return _globalAudioSource;
+		}
+	}
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+}
