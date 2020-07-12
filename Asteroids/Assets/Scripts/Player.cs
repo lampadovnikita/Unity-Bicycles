d7759d2958424bec65bda0d72dca5da92ee98bf6
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-12 23:35:28 +0700

    Remove redundant coroutine for destruction and isAlive flag

--- Asteroids/Assets/Scripts/Player.cs
+++ Asteroids/Assets/Scripts/Player.cs
@@ -36,8 +36,6 @@ public class Player : MonoBehaviour
 
 	private Rigidbody2D playerRigidbody2D;
 
-	private bool isAlive = true;
-
 	private void Awake()
 	{
 		playerRigidbody2D = GetComponent<Rigidbody2D>();
@@ -45,19 +43,9 @@ public class Player : MonoBehaviour
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		StartCoroutine(DestroyPlayer());
-	}
-
-	public IEnumerator DestroyPlayer()
-	{
-		isAlive = false;
-		playerRigidbody2D.velocity = Vector2.zero;
-
 		if (destroyAudioClip != null)
 		{
 			AudioManager.Instance.GlobalAudioSource.PlayOneShot(destroyAudioClip);
-
-			yield return new WaitForSeconds(destructionTime);
 		}
 
 		if (onPlayerDestroyCallback != null)
@@ -68,59 +56,55 @@ public class Player : MonoBehaviour
 		gameObject.SetActive(false);
 	}
 
+
 	public void AddRelativeForce(ForceDirection forceDirection)
 	{
-		if (isAlive == true)
+
+		Vector2 forceDirectionVector = Vector2.zero;
+		switch (forceDirection)
 		{
-			Vector2 forceDirectionVector = Vector2.zero;
-			switch (forceDirection)
-			{
-				case ForceDirection.Up:
-					forceDirectionVector = Vector2.up;
-					break;
-				case ForceDirection.Down:
-					forceDirectionVector = Vector2.down;
-					break;
-				default:
-					Debug.Assert(false, "Unprocessed force direction case!");
-					break;
-			}
+			case ForceDirection.Up:
+				forceDirectionVector = Vector2.up;
+				break;
+			case ForceDirection.Down:
+				forceDirectionVector = Vector2.down;
+				break;
+			default:
+				Debug.Assert(false, "Unprocessed force direction case!");
+				break;
+		}
 
-			playerRigidbody2D.AddRelativeForce(mainForce * Time.deltaTime * forceDirectionVector);
+		playerRigidbody2D.AddRelativeForce(mainForce * Time.deltaTime * forceDirectionVector);
 
-			if (Mathf.Abs(playerRigidbody2D.velocity.x) > maxAxisVelocity)
-			{
-				float newVelocity = (playerRigidbody2D.velocity.x > 0) ? maxAxisVelocity : -1 * maxAxisVelocity;
-				playerRigidbody2D.velocity = new Vector2(newVelocity, playerRigidbody2D.velocity.y);
-			}
+		if (Mathf.Abs(playerRigidbody2D.velocity.x) > maxAxisVelocity)
+		{
+			float newVelocity = (playerRigidbody2D.velocity.x > 0) ? maxAxisVelocity : -1 * maxAxisVelocity;
+			playerRigidbody2D.velocity = new Vector2(newVelocity, playerRigidbody2D.velocity.y);
+		}
 
-			if (Mathf.Abs(playerRigidbody2D.velocity.y) > maxAxisVelocity)
-			{
-				float newVelocity = (playerRigidbody2D.velocity.y > 0) ? maxAxisVelocity : -1 * maxAxisVelocity;
-				playerRigidbody2D.velocity = new Vector2(playerRigidbody2D.velocity.x, newVelocity);
-			}
+		if (Mathf.Abs(playerRigidbody2D.velocity.y) > maxAxisVelocity)
+		{
+			float newVelocity = (playerRigidbody2D.velocity.y > 0) ? maxAxisVelocity : -1 * maxAxisVelocity;
+			playerRigidbody2D.velocity = new Vector2(playerRigidbody2D.velocity.x, newVelocity);
 		}
 	}
 
 	public void AddRotation(RotationDirection rotationDirection)
 	{
-		if (isAlive == true)
+		float rotationDirectionValue = 0f;
+		switch (rotationDirection)
 		{
-			float rotationDirectionValue = 0f;
-			switch (rotationDirection)
-			{
-				case RotationDirection.Right:
-					rotationDirectionValue = -1f;
-					break;
-				case RotationDirection.Left:
-					rotationDirectionValue = 1f;
-					break;
-				default:
-					Debug.Assert(false, "Unprocessed rotation direction case!");
-					break;
-			}
-
-			playerRigidbody2D.rotation += rotationForce * Time.deltaTime * rotationDirectionValue;
+			case RotationDirection.Right:
+				rotationDirectionValue = -1f;
+				break;
+			case RotationDirection.Left:
+				rotationDirectionValue = 1f;
+				break;
+			default:
+				Debug.Assert(false, "Unprocessed rotation direction case!");
+				break;
 		}
+
+		playerRigidbody2D.rotation += rotationForce * Time.deltaTime * rotationDirectionValue;
 	}
 }
