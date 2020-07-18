b3b2e08740bc21c5b82e3b2e152942832941dacf
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-18 14:33:05 +0700

    Add respawn invulnerability

--- Asteroids/Assets/Scripts/Player.cs
+++ Asteroids/Assets/Scripts/Player.cs
@@ -20,6 +20,9 @@ public class Player : MonoBehaviour
 	public event PlayerDestroy OnPlayerDestroy;
 
 	[SerializeField]
+	private SpriteRenderer playerSpriteRenderer = default;
+
+	[SerializeField]
 	private float mainForce = 1000f;
 
 	[SerializeField]
@@ -31,23 +34,29 @@ public class Player : MonoBehaviour
 	[SerializeField]
 	private float maxAxisVelocity = 42f;
 
+	[SerializeField]
+	private float respawnInvulnerableTime = 3f; // In seconds
+
+	[SerializeField]
+	private int InvulnerableBlinkNumber = 3;
+
 	private Rigidbody2D playerRigidbody2D;
+
+	private bool IsInvulnerable { get; set; }
 
 	private void Awake()
 	{
 		playerRigidbody2D = GetComponent<Rigidbody2D>();
+
+		IsInvulnerable = false;
 	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		if (destroyAudioClip != null)
-		{
-			AudioManager.Instance.GlobalAudioSource.PlayOneShot(destroyAudioClip);
+		if (IsInvulnerable == false)
+		{ 
+			DestroyPlayer();
 		}
-
-		OnPlayerDestroy?.Invoke();
-
-		gameObject.SetActive(false);
 	}
 
 	public void Respawn()
@@ -55,6 +64,7 @@ public class Player : MonoBehaviour
 		transform.position = new Vector3(0f, 0f, transform.position.z);
 		transform.rotation = Quaternion.identity;
 		gameObject.SetActive(true);
+		StartCoroutine(TemporaryInvulnerability());
 	}
 
 	public void AddRelativeForce(ForceDirection forceDirection)
@@ -106,5 +116,41 @@ public class Player : MonoBehaviour
 		}
 
 		playerRigidbody2D.rotation += rotationForce * Time.deltaTime * rotationDirectionValue;
+	}
+
+	private void DestroyPlayer()
+	{
+		if (destroyAudioClip != null)
+		{
+			AudioManager.Instance.GlobalAudioSource.PlayOneShot(destroyAudioClip);
+		}
+
+		OnPlayerDestroy?.Invoke();
+
+		gameObject.SetActive(false);
+	}
+
+	private IEnumerator TemporaryInvulnerability()
+	{
+		IsInvulnerable = true;
+
+		Color spriteColor = playerSpriteRenderer.color;
+		float originalAlpha = spriteColor.a;
+
+		float blinkHalfPeriod = respawnInvulnerableTime / (InvulnerableBlinkNumber * 2);
+		for (int i = 0; i < InvulnerableBlinkNumber; i++)
+		{
+			yield return new WaitForSeconds(blinkHalfPeriod);
+
+			spriteColor.a = 0f;
+			playerSpriteRenderer.color = spriteColor;
+
+			yield return new WaitForSeconds(blinkHalfPeriod);
+
+			spriteColor.a = originalAlpha;
+			playerSpriteRenderer.color = spriteColor;
+		}
+
+		IsInvulnerable = false;
 	}
 }
