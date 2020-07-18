a3264b6df749650924e834bf6386f9878fda67f6
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-18 23:53:40 +0700

    Add main music loop playing

---
+++ Asteroids/Assets/Scripts/MainMusicLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMusicLoader : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip MainMusicTheme = default;
+
+    private void Start()
+    {
+        AudioManager instance = AudioManager.Instance;
+        instance.GlobalAudioSource.clip = MainMusicTheme;
+        instance.GlobalAudioSource.loop = true;
+        instance.GlobalAudioSource.Play();
+    }
+}
