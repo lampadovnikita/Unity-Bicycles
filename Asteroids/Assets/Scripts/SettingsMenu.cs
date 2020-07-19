51ace18628012b1e047563264fea95f204880d28
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-19 22:56:03 +0700

    Add settings menu

---
+++ Asteroids/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer generalAudioMixer = default;
+
+    public void SetGeneralVolume(float volume)
+    {
+        generalAudioMixer.SetFloat("GeneralVolume", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        generalAudioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetEffectsVolume(float volume)
+    {
+        generalAudioMixer.SetFloat("EffectsVolume", volume);
+    }
+
+}
