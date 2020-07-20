84aa663a66a52d929e7bea92e91ab3c9922ea84a
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-20 11:32:02 +0700

    Refactor rename

--- Asteroids/Assets/Scripts/OptionsMenu.cs
+++ Asteroids/Assets/Scripts/OptionsMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class SettingsMenu : MonoBehaviour
+public class OptionsMenu : MonoBehaviour
 {
     [SerializeField]
     private AudioMixer generalAudioMixer = default;
