0a7a3b77d36d778a8f6b65f36da539fff9f49636
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-20 12:06:08 +0700

    Add save of options parameters during one session

--- Asteroids/Assets/Scripts/OptionsMenu.cs
+++ Asteroids/Assets/Scripts/OptionsMenu.cs
@@ -1,26 +1,50 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
+    private static float generalVolume = 0f;
+    private static float musicVolume = 0f;
+    private static float effectsVolume = 0f;
+    
     [SerializeField]
     private AudioMixer generalAudioMixer = default;
+
+    [SerializeField]
+    private Slider generalVolumeSlider = default;
+
+    [SerializeField]
+    private Slider musicVolumeSlider = default;
+
+    [SerializeField]
+    private Slider effectsVolumeSlider = default;
+
+
+    public void Start()
+    {
+        generalVolumeSlider.value = generalVolume;
+        musicVolumeSlider.value = musicVolume;
+        effectsVolumeSlider.value = effectsVolume;
+    }
 
     public void SetGeneralVolume(float volume)
     {
         generalAudioMixer.SetFloat("GeneralVolume", volume);
+        generalVolume = volume;
     }
 
     public void SetMusicVolume(float volume)
     {
         generalAudioMixer.SetFloat("MusicVolume", volume);
+        musicVolume = volume;
     }
 
     public void SetEffectsVolume(float volume)
     {
         generalAudioMixer.SetFloat("EffectsVolume", volume);
+        effectsVolume = volume;
     }
 
 }
