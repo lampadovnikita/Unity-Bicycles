c4711184f2f71989546f1583d8909be8a74c3486
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-28 16:18:32 +0700

    Add shoot cooldown

--- Asteroids/Assets/Scripts/Behavior/ShootBehavior.cs
+++ Asteroids/Assets/Scripts/Behavior/ShootBehavior.cs
@@ -9,7 +9,17 @@ public class ShootBehavior : MonoBehaviour
 	private float shootForce = 1500f;
 
 	[SerializeField]
+	private float shootCooldownTime = 0.25f; // In seconds
+
+	[SerializeField]
 	private AudioClip shootAudioClip = default;
+
+	private float canShootTime; // In seconds
+
+	private void Awake()
+	{
+		canShootTime = 0f;
+	}
 
 	private void OnDrawGizmos()
 	{
@@ -18,26 +28,29 @@ public class ShootBehavior : MonoBehaviour
 
 	public void Shoot()
 	{
-		GameObject bullet = bulletPool.GetObject();
-		bullet.transform.position = gameObject.transform.position;
-		bullet.transform.rotation = gameObject.transform.rotation;
-
-		Rigidbody2D bulletRigidbody2D = bullet.GetComponent<Rigidbody2D>();
-		if (bulletRigidbody2D != null)
+		if (Time.time > canShootTime)
 		{
-			bulletRigidbody2D.rotation = gameObject.transform.rotation.eulerAngles.z;
-			bulletRigidbody2D.AddRelativeForce(shootForce * Vector2.up);
-		}
-		else
-		{
-			Debug.LogError("Bullet has no Rigidbody2D component!");
-			return;
-		}
+			canShootTime = Time.time + shootCooldownTime;
 
+			GameObject bullet = bulletPool.GetObject();
+			bullet.transform.position = gameObject.transform.position;
+			bullet.transform.rotation = gameObject.transform.rotation;
 
-		if (shootAudioClip != null)
-		{
-			AudioManager.Instance.EffectsAudioSource.PlayOneShot(shootAudioClip);
+			Rigidbody2D bulletRigidbody2D = bullet.GetComponent<Rigidbody2D>();
+			if (bulletRigidbody2D != null)
+			{
+				bulletRigidbody2D.rotation = gameObject.transform.rotation.eulerAngles.z;
+				bulletRigidbody2D.AddRelativeForce(shootForce * Vector2.up);
+			}
+			else
+			{
+				Debug.LogError("Bullet has no Rigidbody2D component!");
+			}
+ 
+			if (shootAudioClip != null)
+			{
+				AudioManager.Instance.EffectsAudioSource.PlayOneShot(shootAudioClip);
+			}
 		}
 	}
 }
