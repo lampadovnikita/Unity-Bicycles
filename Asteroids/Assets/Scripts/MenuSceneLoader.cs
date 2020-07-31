b3b25e8399731fb9550a4d7a34657e62499742a5
Nikita Lampadov <lampadovnikita@gmail.com>
2020-07-31 21:38:17 +0700

    Add Preload scene for sigleton gameobjects

---
+++ Asteroids/Assets/Scripts/MenuSceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneLoader : MonoBehaviour
+{
+    private const int MENU_SCENE_INDEX = 1;
+
+    void Start()
+    {
+        SceneManager.LoadScene(MENU_SCENE_INDEX);
+    }
+}
